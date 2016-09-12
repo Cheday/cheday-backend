d8633768679a46318a0b50c0e410b2d1ddd1ad52
Andrey Krukovskiy <andrey.krukovskiy@gmail.com>
2016-09-12 13:33:33 +0300

    Удаление связанных объектов (требуемых волонтеров), при удалении события

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,9 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.afterDelete("Event", function(req) {
+	var volonteerRolesWithCountArray = req.object.get("volonteerRoles");
+	Parse.Object.destroyAll(volonteerRolesWithCountArray).then(function(){
+		},
+		function(error){
+			console.error("Error deleteing related VolonteerRolesWithCount " + error.code + ": " + error.message);
+		});
 });
