57ee7ae5d0d0d9a6c7de2f4cc2b06e19c796516d
Andrey Krukovskiy <andrey.krukovskiy@gmail.com>
2016-10-07 17:41:47 +0300

    Удаление файлов при удалении события

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,4 @@
+var Config = require('parse-server/lib/Config');
 
 Parse.Cloud.afterDelete("Event", function(req) {
 	var volonteerRolesWithCountArray = req.object.get("volonteerRoles");
@@ -5,5 +6,13 @@ Parse.Cloud.afterDelete("Event", function(req) {
 		},
 		function(error){
 			console.error("Error deleteing related VolonteerRolesWithCount " + error.code + ": " + error.message);
+		}).then(function(){
+			const config = new Config(Parse.applicationId);
+		    const filesController = config.filesController;
+		    return filesController.deleteFile(config, req.object.get("image").name);
+		}).then(function(result){
+			console.log("Result of deletion file: " + JSON.stringify(result));
+		}, function(error){
+			console.error("Error deleting event image " + error.code + ": " + error.message);
 		});
 });
